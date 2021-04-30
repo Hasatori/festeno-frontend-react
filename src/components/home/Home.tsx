856a29352e2ff9457fe7e80407c2bd0341c4bdb6
Hasatori <hradil.o@email.cz>
2021-04-30 18:29:37 +0200

    Default value of the watchingStream hook changed.

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     const streamers = new Array(50).fill('');
     const recipesForRow = new Array(4).fill('Recipe');
     const rows = new Array(6).fill('Recipe');
-    const [watchingStreamer, setWatchingStreamer] = useState(true);
+    const [watchingStreamer, setWatchingStreamer] = useState(false);
     const [streamerImg, setStreamerImg] = useState(`https://mdbootstrap.com/img/Photos/Avatars/avatar-${Math.floor(Math.random() * 15) + 1}.jpg`);
     const [comments, setComments] = useState<string[]>(["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]);
     const [comment, setComment] = useState("");
