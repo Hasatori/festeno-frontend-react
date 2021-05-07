d19e9df330bb0986cc7b2e2e77e78244311e2e52
Hasatori <hradil.o@email.cz>
2021-05-07 21:55:36 +0200

    Recipes layout created.

--- src/components/recipes/Recipes.tsx
+++ src/components/recipes/Recipes.tsx
@@ -14,7 +14,7 @@ const ingredients: Ingredient[] = [
     {name: "kokos strouhaný", unit: "200g"},
     {name: "cukrářské piškoty", unit: "300g"},
 ]
-const process:string[] = new Array(8).fill('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
+const process: string[] = new Array(8).fill('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
 
 export function Recipes() {
     const items = ["video", "image"];
@@ -23,6 +23,8 @@ export function Recipes() {
     const [protein, setProtein] = useState(0);
     const [fat, setFat] = useState(0);
     const [fiber, setFiber] = useState(0);
+    const [settingDetails, setSettingDetails] = useState(false);
+    const types = ['Bio', 'Raw', 'Vegetarian', 'Vegan', 'Low carb', 'Paleo', 'Low fat', 'Lacto free'];
     const nutrientRows = [
         {
             name: "Carbohydrate",
@@ -98,16 +100,75 @@ export function Recipes() {
                     <MDBCol md="6" xl="4">
                         <div className="d-flex flex-column mt-3 border-grey rounded p-3">
                             <div className="d-flex flex-row justify-content-around mb-3">
-                                <div className="mr-3">Details <MDBIcon icon="sliders-h" /></div>
-                                <div>Set nutrients <MDBIcon icon="sliders-h" /></div>
+                                <div
+                                    className={settingDetails ? "d-flex flex-row details-change-section-button-active" : "d-flex flex-row details-change-section-button"}
+                                    onClick={() => setSettingDetails(true)}>
+                                    <div className="align-self-center">Details</div>
+                                    <div><MDBIcon className="ml-3" icon="sliders-h" size="2x"/></div>
+                                </div>
+                                <div
+                                    className={!settingDetails ? "d-flex flex-row details-change-section-button-active" : "d-flex flex-row details-change-section-button"}
+                                    onClick={() => setSettingDetails(false)}>
+                                    <div className="align-self-center">Set nutrients</div>
+                                    <div><MDBIcon className="ml-3" icon="sliders-h" size="2x"/></div>
+                                </div>
                             </div>
-                            <div className='d-flex flex-column'>
+                            {settingDetails &&
+                            <>
+                                <div className='d-flex flex-column'>
 
-                                {nutrientRows.map((nutrient, index, array) => {
-                                    return (<NutrientRow {...nutrient.name} />)
-                                })}
-                            </div>
-                            <div className="nav-button text-center">Generate from ingredients</div>
+                                    {nutrientRows.map((nutrient, index, array) => {
+                                        return (<NutrientRow {...nutrient.name} />)
+                                    })}
+                                </div>
+                                <div className="nav-button align-self-center">Generate from ingredients</div>
+                            </>
+                            }
+                            {!settingDetails &&
+                            <>
+                                <div className='d-flex flex-column '>
+                                    <MDBRow >
+                                        <MDBCol size="2"/>
+                                        <MDBCol size="4">Time</MDBCol>
+                                        <MDBCol size="4"><input
+                                            type="text"
+                                            className="form-control"
+                                            id="formGroupExampleInput"
+                                        /></MDBCol>
+                                        <MDBCol size="2"/>
+                                    </MDBRow>
+                                    <MDBRow className="mt-2">
+                                        <MDBCol size="2"/>
+                                        <MDBCol size="4">Kcal</MDBCol>
+                                        <MDBCol size="4"><input
+                                            type="text"
+                                            className="form-control"
+                                            id="formGroupExampleInput"
+                                        /></MDBCol>
+                                        <MDBCol size="2"/>
+                                    </MDBRow>
+                                    <MDBRow className="mt-2">
+                                        <MDBCol size="2"/>
+                                        <MDBCol size="4">Type</MDBCol>
+                                        <MDBCol size="4">      <div>
+                                            <select className="browser-default custom-select">
+                                                <option value="1">Breakfast</option>
+                                                <option value="2">Lunch</option>
+                                                <option value="3">Snack</option>
+                                                <option value="4">Dinner</option>
+                                            </select>
+                                        </div></MDBCol>
+                                        <MDBCol size="2"/>
+                                    </MDBRow>
+                                    <div>
+                                        {types.map((val) => {
+                                            return <MDBBtn className='neutral-button'>{val}</MDBBtn>
+                                        })}
+                                    </div>
+                                </div>
+
+                            </>
+                            }
                         </div>
 
                     </MDBCol>
@@ -135,15 +196,15 @@ export function Recipes() {
                     <MDBCol xl="4" lg="12">
                         <div className="d-flex flex-column mt-3">
                             <div className="h4-responsive mb-3">Process</div>
-                            {process.map((step,index)=>{
+                            {process.map((step, index) => {
                                 return (
                                     <MDBRow className="d-flex flex-row ">
                                         <MDBCol size="1" className="text-center">{++index}</MDBCol>
                                         <MDBCol size="10"
                                                 className="border-grey rounded text-center p-3 long-text">{step}</MDBCol>
-                                        <MDBCol  size="1"
-                                                 className="text-center p-2"><MDBIcon size="1x"
-                                                                                      icon="trash-alt"/></MDBCol>
+                                        <MDBCol size="1"
+                                                className="text-center p-2"><MDBIcon size="1x"
+                                                                                     icon="trash-alt"/></MDBCol>
                                     </MDBRow>
                                 )
                             })}
@@ -154,31 +215,30 @@ export function Recipes() {
             </>
             }
             {location.hash == "#upload-from-existing-page" &&
-                <MDBContainer>
-            <MDBCol lg="6" md="12" className="d-flex flex-column">
+            <MDBContainer>
+                <MDBCol lg="6" md="12" className="d-flex flex-column">
 
-                <div className="form-group">
-                    <label htmlFor="formGroupExampleInput">Link to the website from which you want to upload a
-                        recipe</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                    />
-                </div>
-                <div className="mt-2">
-                    <div className="nav-button-active">Add</div>
-                </div>
-            </MDBCol>
-                </MDBContainer>
+                    <div className="form-group">
+                        <label htmlFor="formGroupExampleInput">Link to the website from which you want to upload a
+                            recipe</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="mt-2">
+                        <div className="nav-button-active">Add</div>
+                    </div>
+                </MDBCol>
+            </MDBContainer>
             }
         </div>
     )
 }
 
-interface Ingredient
-{
+interface Ingredient {
     name: string,
-        unit
-:
-    string
+    unit
+        :
+        string
 }
