82c43cc04d940f8ee78460fd60e8671e5e2cdeaa
Hasatori <hradil.o@email.cz>
2021-10-09 13:18:25 +0200

    Feed layout updated.

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import './Home.css';
-import {MDBCol, MDBContainer, MDBRow} from "mdbreact";
+import {MDBCol, MDBRow} from "mdbreact";
 import {Link} from "react-router-dom";
 import {useMediaQuery} from "react-responsive";
 import heart_empty from "../../assets/images/common/heart_empty.svg";
@@ -37,6 +37,7 @@ function mapStateToProps(state: AppState, props: HomeProps) {
 function Home(props: HomeProps) {
     const rows = new Array(6).fill(null);
     const isSmallScreen = useMediaQuery({query: '(max-width: 700px)'});
+    const starWidth = 25;
 
     useEffect(() => {
         props.loadFeed();
@@ -47,71 +48,68 @@ function Home(props: HomeProps) {
             <div
                 className={'d-flex flex-column pt-4 pl-2'}>
                 <div className='flex-column mx-2'>
-                    { rows.map(value => {
-                        return (
-                            <div className='mt-3'>
-                                <MDBContainer fluid={false}>
-                                    <MDBRow>
-                                        {props.feed.map((recipe: Recipe, index) => {
-                                            return (
-                                                <MDBCol md='6' xl='3' o sm="12" className='mt-5 px-4'>
-                                                    <Link to={`${Routes.RECIPE}/adasdad54536s4fg65ds4fa5s4f`}>
-                                                        <div className='d-flex flex-column recipe-wrapper z-depth-1'>
+                    {
+                        <div className='mt-3'>
+                            <MDBRow>
+                                {props.feed.map((recipe: Recipe, index) => {
+                                    return (
+                                        <MDBCol md='6' xl='3' o sm="12" className='mt-5 px-4'>
+                                            <Link to={`${Routes.RECIPE}/adasdad54536s4fg65ds4fa5s4f`}>
+                                                <div className='d-flex flex-column recipe-wrapper z-depth-1'>
 
-                                                            <div><img className='recipe-image'
-                                                                      src={`data:${recipe.layoutImage.type};base64,${recipe.layoutImage.data}`}/>
-                                                            </div>
-                                                            <div className='recipe-footer d-flex flex-column'>
-                                                                <div className="mb-3 h6-responsive">{recipe.title}</div>
-                                                                <div
-                                                                    className="d-flex flex-row justify-content-between">
-                                                                    <div className="d-flex flex-row">
-                                                                        <div className="mr-1">
-                                                                            <img
-                                                                                src={`data:${recipe.author.profileImage.type};base64,${recipe.author.profileImage.data}`}
-                                                                                className='recipe-author-image'
-                                                                                alt="aligment"/>
-                                                                        </div>
-                                                                        <div className="d-flex flex-column">
-                                                                            <small>{recipe.author.name}</small>
-                                                                            <small>
-                                                                                2m
-                                                                            </small>
-                                                                        </div>
-                                                                    </div>
-
-                                                                    <div className="d-flex flex-row">
-                                                                        <div className="mr-1">
-                                                                            {new Array(recipe.rating).fill(0).map(()=>{
-                                                                                  return  <img
-                                                                                        src={star_filled}
-                                                                                        width={27}/>
-                                                                                })}
-                                                                            {new Array(5-recipe.rating).fill(0).map(()=>{
-                                                                                    return  <img
-                                                                                        src={star_empty}
-                                                                                        width={27}/>
-                                                                                })}
-                                                                          </div>
-                                                                        <div className="ml-1"><img
-                                                                            src={index % 2 == 0 ? heart_empty : heart_filled}
-                                                                            width={25}/></div>
-
-                                                                    </div>
-
+                                                    <div><img className='recipe-image'
+                                                              src={`data:${recipe.layoutImage.type};base64,${recipe.layoutImage.data}`}/>
+                                                    </div>
+                                                    <div className='recipe-footer d-flex flex-column'>
+                                                        <div className="mb-3 h6-responsive">{recipe.title}</div>
+                                                        <div
+                                                            className="d-flex flex-column justify-content-between">
+                                                            <div className="d-flex flex-row">
+                                                                <div className="mr-1">
+                                                                    <img
+                                                                        src={`data:${recipe.author.profileImage.type};base64,${recipe.author.profileImage.data}`}
+                                                                        className='recipe-author-image'
+                                                                        alt="aligment"/>
                                                                 </div>
+                                                                <div className="d-flex flex-column">
+                                                                    <small>{recipe.author.name}</small>
+                                                                    <small>
+                                                                        2m
+                                                                    </small>
+                                                                </div>
+                                                            </div>
+
+                                                            <div className="mt-3 d-flex flex-row align-self-end">
+                                                                <div className="mr-1">
+                                                                    {new Array(recipe.rating).fill(0).map(() => {
+                                                                        return <img
+                                                                            src={star_filled}
+                                                                            width={starWidth}/>
+                                                                    })}
+                                                                    {new Array(5 - recipe.rating).fill(0).map(() => {
+                                                                        return <img
+                                                                            src={star_empty}
+                                                                            width={starWidth}/>
+                                                                    })}
+                                                                </div>
+                                                                <div className="ml-1"><img
+                                                                    src={index % 2 == 0 ? heart_empty : heart_filled}
+                                                                    width={25}/></div>
 
                                                             </div>
-                                                        </div>
-                                                    </Link>
-                                                </MDBCol>
-                                            )
-                                        })}
-                                    </MDBRow></MDBContainer>
-                            </div>
-                        )
 
-                    })}
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </MDBCol>
+                                    )
+                                })}
+                            </MDBRow>
+                        </div>
+
+                    }
 
                 </div>
             </div>
