0bf02338e88d09bc28b241f944ad5b3e7404ab1f
Hasatori <hradil.o@email.cz>
2022-04-06 18:42:14 +0200

    Redirect for default route added.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -161,6 +161,7 @@ function App(appProps: AppProps) {
                 {(showMenu ? <div><AppHeader {...appProps}/></div> : <></>)}
                 <div className='flex-grow-1 app-body'>
                     <Switch>
+                        <Route exact path={"/"} render={()=> <Redirect to={Routes.ABOUT}/>} />
                         <Route exact path={Routes.EXPLORE} component={Explore}/>
                         <Route exact path={Routes.ABOUT} component={About}/>
                         <Route path={Routes.RECIPES} render={(props) => <Recipes  {...appProps} />}/>
