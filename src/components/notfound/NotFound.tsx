10d4df716823661ad998a1b54918bb84a8393ff5
Hasatori <hradil.o@email.cz>
2022-05-29 14:07:40 +0200

    Not found section updated.

--- src/components/notfound/NotFound.tsx
+++ src/components/notfound/NotFound.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
+import './NotFound.css';
 import {Link} from 'react-router-dom';
 import {useTranslation} from "react-i18next";
-import {MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardText, MDBCardTitle} from 'mdbreact';
+import {MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardText, MDBCardTitle, MDBContainer} from 'mdbreact';
 import {Routes} from "../../util/Constants";
 
 function NotFound() {
     const {t} = useTranslation();
     return (
-        <MDBCard style={{ width: "22rem" }} className='m-auto'>
 
-            <MDBCardBody>
-                <MDBCardTitle className="text-center h1-responsive">404</MDBCardTitle>
-                <MDBCardText className='text-center'>
-                    {t('ns1:pageNotFoundMessage')}
-                </MDBCardText>
-                <Link to={Routes.FEED} >
-                <MDBBtn className="flex-center" color="primary" href="#">{t('ns1:goBackButton')}</MDBBtn>
-                </Link>
-            </MDBCardBody>
-        </MDBCard>
-    );
+        <div>
+            <div className="d-flex flex-center mt-5 p-2"><h1>404</h1></div>
+            <div className="d-flex flex-center  p-2"><h2>{t('ns1:pageNotFoundMessage')}</h2></div>
+            <div className="d-flex flex-center  p-2">
+                <Link to={Routes.ABOUT}>
+                    <MDBBtn className="background-color-primary color-background rounded bold">{t('ns1:goBackButton')}</MDBBtn>
+                </Link></div>
+        </div>);
 }
 
 export default NotFound;
